1044c9ab530546187af986259ec428b90b7c0f78
Dr. Németh Tamás <tomuwhu@gmail.com>
2025-03-19 12:02:52 +0100

    first commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,63 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Malom Játék</h1>
+
+<table>
+    <tbody>
+        <tr>
+            <td colspan="3" rowspan="3" class="b">
+                <div class="p"></div>
+            </td>
+            <td></td>
+            <td colspan="3" rowspan="3" class="b">
+                <div class="p p2"></div>
+            </td>
+            <td></td>
+            <td colspan="3" rowspan="3" class="b">
+                <div class="p p1"></div>
+            </td>
+        </tr>
+        <tr>
+            <td class="vcs"></td>
+            <td class="vcs"></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+    </tbody>
+</table>
+
+<style>
+    table {
+        margin: auto;
+        border-collapse: separate;
+        border-spacing: 0px;
+    }
+    h1 {
+        text-align: center;
+    }
+    td.vcs {
+        background-color: black;
+        height: 6px;
+        width: 100px;
+        background-color: rgb(190, 190, 190);
+        border: 1px solid black;
+    }
+    td.b {
+        border: 1px solid black;
+        border-radius: 10px;
+        width: 30px;
+        height: 30px;
+        background-color: rgb(190, 190, 190);
+    }
+    div.p {
+        width: 100%;
+        height: 100%;
+        border-radius: 15px;
+    }
+    div.p1 {
+        background-color: red;
+    }
+    div.p2 {
+        background-color: blue;
+    }
+</style>
